eb6c5d939ee33484ffcca1cec389f5051288e3df
php 17 <php.17@dino.cec.ime.usp.br>
2018-01-23 22:37:23 -0200

    Exercicio Metodo deposita e saca

--- Conta.php
+++ Conta.php
@@ -2,9 +2,24 @@
 
 class Conta{
   public $numero;
-  public $saldo = 0;
+  public $saldo = 1000;
+  public $limite = 100;
+  public $agencia;
 
   function deposita(float $valor){
     $this->saldo += $valor;
   }
+
+  function saca(float $valor){
+    $this->saldo -= $valor;
+  }
+
+  function consultaSaldo(){
+    return $this->saldo;
+  }
+
+  function imprimeExtrato($num){
+    return $this->saldo . " " . $this->saldo;
+  }
+
 }
--- TestaConta.php
+++ TestaConta.php
@@ -1,9 +1,15 @@
 <?php
 declare(strict_types = 1);
-
 require 'Conta.php';
+
 $conta = new Conta();
 echo"Saldo inicial da conta: $conta->saldo" . PHP_EOL;
 $conta->deposita(2000);
 
 echo"Saldo final da conta: $conta->saldo" . PHP_EOL;
+
+$conta->saca(250);
+echo"Saldo final da conta: $conta->saldo" . PHP_EOL;
+
+$ret = $conta->consultaSaldo();
+echo "Consulta Saldo: $ret" . PHP_EOL;
